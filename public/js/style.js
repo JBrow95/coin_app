4d0843a657713efc994de41b36f3baf04cf9ff77
JBrow95 <browningjustin95@gmail.com>
2018-07-25 14:25:51 -0400

    fixed issue with enter not displaying alert box

--- public/js/style.js
+++ public/js/style.js
@@ -1,17 +1,22 @@
+
 function yourWelcome() {
 if (document.getElementById("welcome").value != "") {
 	alert(new Date().toLocaleString());
 	document.getElementById("time").submit();
-	
-}else {
-	alert("Welcome to Coin Changer" + " " + new Date().toLocaleString());
  };
-}
+ if (document.getElementById("welcome").value == "") {
+ 	alert("Welcome to coin exchange! Enter a Value");
+ };
+};
+
+function offFunc() {
+	alert(new Date().toLocaleString());
+};
 
 
 function showTime() {
 	document.getElementById("showTime").innerHTML = new Date().toLocaleString();
-}
+};
 
 function randombg(){
   var random= Math.floor(Math.random() * 2) + 0;
@@ -19,11 +24,22 @@ function randombg(){
              	"urk('https://images.pexels.com/photos/375732/pexels-photo-375732.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=650&amp;w=940')"
               ];
   document.getElementById("random").style.backgroundImage=bigSize[random];
-}
+};
 
 
 function reveal() {
 	showTime();
 	randombg();
+};
 
-}
+
+function enterFunction() {
+
+	document.getElementById("welcome")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+ 	document.getElementById("hover").click();
+ 	yourWelcome();
+    }
+})};
