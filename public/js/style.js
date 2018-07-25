e98b66be9deb4d760d8d84c3aedf8c9f7e5f98be
JBrow95 <browningjustin95@gmail.com>
2018-07-25 11:03:41 -0400

    created js function to change background on load

--- public/js/style.js
+++ public/js/style.js
@@ -1,6 +1,6 @@
 function yourWelcome() {
 if (document.getElementById("welcome").value != "") {
-	confirm("Do you want to submit?" + " " + new Date().toLocaleString());
+	alert(new Date().toLocaleString());
 	document.getElementById("time").submit();
 	
 }else {
@@ -8,3 +8,22 @@ if (document.getElementById("welcome").value != "") {
  };
 }
 
+
+function showTime() {
+	document.getElementById("showTime").innerHTML = new Date().toLocaleString();
+}
+
+function randombg(){
+  var random= Math.floor(Math.random() * 2) + 0;
+  var bigSize = ["url('https://images.pexels.com/photos/36026/amazing-beautiful-beauty-blue.jpg?auto=compress&amp;cs=tinysrgb&amp;h=650&amp;w=940')",               
+             	"urk('https://images.pexels.com/photos/375732/pexels-photo-375732.jpeg?auto=compress&amp;cs=tinysrgb&amp;h=650&amp;w=940')"
+              ];
+  document.getElementById("random").style.backgroundImage=bigSize[random];
+}
+
+
+function reveal() {
+	showTime();
+	randombg();
+
+}
